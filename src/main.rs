400e9a553701c9fd8a6455b7ebbbe85875fdfca4
Dmytro Brazhnyk <amid.ukr@gmail.com>
2023-10-06 21:53:24 -0500

    Adf to avro converter

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,126 @@ extern crate core;
 
 use std::io;
 use serde_json::value::Value;
-use std::fmt::Write;
+use serde_json::Map;
 
-fn do_something(output: &mut String) {
-    writeln!(output, "Ha-ha-ha").unwrap();
+trait ValueExt {
+    fn as_array(&self) -> Option<&Vec<Value>>;
+    fn as_object_value(&self) -> Option<&Map<String, Value>>;
+    fn as_string(&self) -> Option<&String>;
 }
 
-fn convert_avro_2_adf_schema(avro: &str) -> String {
-    let mut output = String::new();
+impl ValueExt for Value {
+    fn as_array(&self) -> Option<&Vec<Value>> {
+        return  match self {
+            Value::Array(o) => Some(o),
+            _ => None
+        }
+    }
 
-    writeln!(output, "He-he-he").unwrap();
-    do_something(&mut output);
+    fn as_object_value(&self) -> Option<&Map<String, Value>> {
+        return  match self {
+            Value::Object(o) => Some(o),
+            _ => None
+        }
+    }
 
-    let v: Value = serde_json::from_str(avro).unwrap();
+    fn as_string(&self) -> Option<&String> {
+        return  match self {
+            Value::String(o) => Some(o),
+            _ => None
+        }
+    }
+}
 
-    return output
+fn handle_type(ident: usize, json: &Value) {
+    match json {
+        Value::Null => {}
+        Value::String(o) => handle_string_type(o),
+        Value::Array(o) => handle_array_type(ident, o),
+        Value::Object(o) => handle_map_type(ident, o),
+        _ => panic!("Unexpected avro type tag: {}", json),
+    }
+}
+
+fn handle_map_type(ident: usize, object: &Map<String, Value>) {
+
+    if let Some(Value::String(t)) = object.get("type") {
+        match t.as_str() {
+            "record" => {handle_record_type(ident, object); return;},
+            _ => {}
+        }
+    }
+
+    panic!("Unexpected type definition = {:?}", object);
+}
+
+fn handle_record_type(ident: usize, object: &Map<String, Value>) {
+    let fields = object.get("fields")
+        .expect(format!("`record` type expect to have 'fields' attribute, json = {:?}", object).as_str());
+
+    let field_list = fields.as_array()
+        .expect(format!("`fields` attribute expected to be of json array type, fields = {}", fields).as_str());
+
+    
+    println!("{: <1$}(", "", ident);
+
+    for (pos, e) in field_list.iter().enumerate() {
+
+        let field = e.as_object_value()
+            .expect(format!("Elements of `fields` attributes are expected to be of json object type, fields element = {}", e).as_str());
+
+        let name = field.get("name")
+            .expect(format!("Field element expected to have json `name` attribute, fields element = {:?}", field).as_str())
+            .as_str()
+            .expect(format!("`name` field element expected to be of json string type, fields element = {:?}", field).as_str());
+
+        let _type = field.get("type")
+            .expect(format!("Field element expected to have json `type` attribute, field = {:?}", field).as_str());
+
+        print!("{: <1$}{name} as ", "", ident + 4);
+
+        handle_type(ident + 4, _type);
+
+        if pos != field_list.len() - 1 {
+            println!(",")
+        }else{
+            println!()
+        }
+    }
+
+    println!("{: <1$})", "", ident);
+}
+
+fn handle_array_type(ident: usize, array: &Vec<Value>) {
+    let type_array:Vec<_> = array.iter()
+        .filter(|t| match t {
+            Value::String(s) if s != "null" => true,
+            _ => true
+        }
+        ).collect();
+
+    if type_array.len() != 1 {
+        panic!("Array of types has different to 1 of non-nullable types, {:?}", array);
+    }
+
+    handle_type(ident, type_array.into_iter().next()
+        .unwrap())
+}
+
+fn handle_string_type(t: &String)  {
+    print!("{}", t)
+}
+
+fn convert_avro_2_adf_schema(avro: &str)  {
+    let root: Value = serde_json::from_str(avro).unwrap();
+
+    handle_type(0, &root)
 }
 
 fn main() -> io::Result<()>{
     let json_input = io::read_to_string(io::stdin())?;
 
-    println!("{}", json_input);
-
-    // println!("{}", convert_avro_2_adf_schema(json_input.as_str()));
+    convert_avro_2_adf_schema(json_input.as_str());
 
     Ok(())
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,8 +4,6 @@ use assert_cmd::Command;
 use test_case::test_case;
 use std::fs;
 
-use std::fmt::{format, Write};
-
 fn command() -> Command {
     return Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
 }
@@ -19,18 +17,19 @@ fn read_test_resource(file_name: &str) -> String {
         .expect(format!("Unable to read {} file", file_name).as_str());
 }
 
-#[test_case("test"; "main test case")]
-fn avro2adf_test(testCase: &str){
-
-
-    let mut adf = read_test_resource(format!("{}-adf.txt", testCase).as_str());
-    let mut avro = read_test_resource(format!("{}-avro.json", testCase).as_str());
+#[test_case("simple-object"; "simple object test case")]
+fn avro2adf_test(test_case: &str){
+    let mut adf = read_test_resource(format!("{}-adf.txt", test_case).as_str());
+    let mut avro = read_test_resource(format!("{}-avro.json", test_case).as_str());
 
     adf = adf.trim().to_string();
-    avro = adf.trim().to_string();
+    avro = avro.trim().to_string();
+
+    adf = adf.replace("\r\n", "\n");
     adf.push('\n');
 
     let mut command = command();
+
     command
         .write_stdin(avro)
         .assert()
